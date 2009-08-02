59126214c16aabab11a04d7516a1f40d4116ac55
Sebastian Riedel <kraihx@googlemail.com>
2009-08-02 00:14:17 +0000

    Import of SRI/Mojo-Server-FCGI-0.16 from CPAN.
    gitpan-cpan-distribution: Mojo-Server-FCGI
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         SRI/Mojo-Server-FCGI-0.16.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

--- lib/Mojo/Server/FCGI.pm
+++ lib/Mojo/Server/FCGI.pm
@@ -12,7 +12,7 @@ use FCGI;
 
 use constant CHUNK_SIZE => $ENV{MOJO_CHUNK_SIZE} || 4096;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 # Wow! Homer must have got one of those robot cars!
 # *Car crashes in background*
